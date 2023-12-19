904319a0b35803ca3e5357e076aa4d69715fec6d
Kiash254 <codewithkiash@gmail.com>
2023-12-20 00:21:31 +0300

    added formatted views files

---
+++ stream.py
@@ -0,0 +1,12 @@
+print("Hello this is  stream")
+import streamlit as st
+
+st.title("HEllo welcome  to my stream lit App")
+st.header("Hello Hello  am kiash")
+st.subheader('Hello am The Current Developer on Model UIS')
+st.text("Hello  here is the text enev")
+st.markdown("*** Helllo** am The current dev")
+st.markdown("[Google linke]google.com")
+st.markdown("---")
+st.caption("This is my caption")
+st.latex(r"\begein{pmat}")
